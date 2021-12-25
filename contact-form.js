8a5f4dadf7e80408406c9b1e08680d22c155e344
Eejit <76887639+Eejit43@users.noreply.github.com>
2021-12-25 02:03:00 -0500

    finally finished contact form (merry christmas!)

--- contact-form.js
+++ contact-form.js
@@ -1,15 +1,113 @@
 window.onload = function () {
-  document.getElementById('submitit').addEventListener("click", submitForm);
+  document.getElementById('name').addEventListener("input", checkNameField);
+  document.getElementById('name').addEventListener("blur", checkNameField);
+  document.getElementById('email').addEventListener("input", checkEmailField);
+  document.getElementById('email').addEventListener("blur", checkEmailField);
+  document.getElementById('message').addEventListener("input", checkMessageField);
+  document.getElementById('message').addEventListener("blur", checkMessageField);
+  document.getElementById('send').addEventListener("click", submitForm);
 }
 
-function showAlert(id) {
+function setRedBorder(id) {
   let element = document.getElementById(id);
-  element.className = "alert show";
-  setTimeout(function () {
-    element.className = element.className.replace("alert show", "alert");
-  }, 2000);
+  element.style.border = "1px solid #ff5555";
+}
+
+function resetBorder(id) {
+  let element = document.getElementById(id);
+  element.style.border = "";
+}
+
+let nameValid, emailValid, messageValid = 0; // 0 = unset, 1 = not valid, 2 = valid
+let validEmail;
+
+function checkNameField() {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+  const errorMsg = document.getElementById('error-msg');
+  if (name.length === 0) {
+    setRedBorder('name');
+    errorMsg.innerHTML = 'Please fill out missing fields!<br>';
+    nameValid = 1;
+  } else if (name.length > 0 && email.length > 0 && message.length > 0 && validEmail === true) {
+    errorMsg.innerHTML = '';
+    resetBorder('name');
+    nameValid = 2;
+  } else if (name.length > 0 && email.length > 0 && validEmail === false) {
+    resetBorder('name');
+    errorMsg.innerHTML = 'Invalid email address!<br>';
+    messageValid = 2;
+  } else if (name.length > 0) {
+    resetBorder('name');
+    messageValid = 2;
+  }
+}
+
+function checkEmailField() {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+  const errorMsg = document.getElementById('error-msg');
+  validEmail = (/^(?:[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/).test(email);
+  if (validEmail != true) {
+    setRedBorder('email');
+    errorMsg.innerHTML = 'Invalid email address!<br>';
+    emailValid = 1;
+  } else if (nameValid === 1 || messageValid === 1) {
+    errorMsg.innerHTML = 'Please fill out missing fields!<br>';
+    resetBorder('email');
+    emailValid = 2;
+  } else if (validEmail === true && nameValid === 2 && messageValid === 2) {
+    errorMsg.innerHTML = '';
+    resetBorder('email');
+    emailValid = 2;
+  }
+  if (email.length === 0) {
+    setRedBorder('email');
+    errorMsg.innerHTML = 'Please fill out missing fields!<br>';
+    emailValid = 1;
+  } else if (name.length > 0 && email.length > 0 && message.length > 0 && validEmail === true && document.getElementById('name').style.border != "1px solid #ff5555" && document.getElementById('message').style.border != "1px solid #ff5555") {
+    errorMsg.innerHTML = '';
+    resetBorder('email');
+    emailValid = 2;
+  } else if (email.length > 0 && validEmail === true && document.getElementById('name').style.border != "1px solid #ff5555" && document.getElementById('message').style.border != "1px solid #ff5555") {
+    resetBorder('email');
+    emailValid = 2;
+  }
+}
+
+function checkMessageField() {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+  const errorMsg = document.getElementById('error-msg');
+  if (message.length === 0) {
+    setRedBorder('message');
+    errorMsg.innerHTML = 'Please fill out missing fields!<br>';
+    messageValid = 1;
+  } else if (name.length > 0 && email.length > 0 && message.length > 0 && validEmail === true) {
+    errorMsg.innerHTML = '';
+    resetBorder('message');
+    messageValid = 2;
+  } else if (name.length > 0 && email.length > 0 && validEmail === false) {
+    resetBorder('message');
+    errorMsg.innerHTML = 'Invalid email address!<br>';
+    messageValid = 2;
+  } else if (message.length > 0) {
+    resetBorder('message');
+    messageValid = 2;
+  }
 }
 
 function submitForm() {
+  const errorMsg = document.getElementById('error-msg');
 
+  checkNameField();
+  checkEmailField();
+  checkMessageField();
+
+  if (nameValid === 2 && emailValid === 2 && messageValid === 2 && validEmail === true) {
+    document.getElementById("contact-form").submit();
+  }
 }
