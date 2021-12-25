21936b107285430424afc9a47f2edcfa33330d9f
Eejit <76887639+Eejit43@users.noreply.github.com>
2021-12-25 18:46:19 -0500

    added image popups

---
+++ image-popup.js
@@ -0,0 +1,26 @@
+// Modified from https://jsfiddle.net/snowMonkey/f1zav0ge/
+
+let modal = document.getElementById('modal');
+let images = document.getElementsByClassName('popupimage');
+let modalImg = document.getElementById("enlarged-image");
+let captionText = document.getElementById("caption");
+
+for (let i = 0; i < images.length; i++) {
+  let img = images[i];
+  img.onclick = function (evt) {
+    console.log(evt);
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
+
+let span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+modal.onclick = function () {
+  modal.style.display = "none";
+}
