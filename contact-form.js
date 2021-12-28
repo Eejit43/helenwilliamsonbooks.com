d941e7041ce0add69383157dea241ec7937dfc1a
Eejit <76887639+Eejit43@users.noreply.github.com>
2021-12-28 11:17:57 -0500

    reworked contact form error message system

--- contact-form.js
+++ contact-form.js
@@ -19,28 +19,33 @@ function resetBorder(id) {
 }
 
 let nameValid, emailValid, messageValid = 0; // 0 = unset, 1 = not valid, 2 = valid
-let validEmail;
+let validEmail; // true/false
+let captchaState = 0; // 0 = not completed, 1 = error, 2 = completed
+
+const errorMsg = document.getElementById('error-msg');
+
+function updateErrorMsg() {
+  if (nameValid === 1 || emailValid === 1 || messageValid === 1) {
+    errorMsg.innerHTML = 'Please fill out missing field(s)!<br>';
+  } else if (validEmail === false) {
+    errorMsg.innerHTML = 'Invalid email address!<br>';
+  } else if (captchaState === 1) {
+    errorMsg.innerHTML = 'An error occurred or the captcha expired, please (re)complete!<br>';
+  } else {
+    errorMsg.innerHTML = '';
+  }
+}
 
 function checkNameField() {
   const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const message = document.getElementById('message').value;
-  const errorMsg = document.getElementById('error-msg');
   if (name.length === 0) {
     setRedBorder('name');
-    errorMsg.innerHTML = 'Please fill out missing field(s)!<br>';
     nameValid = 1;
-  } else if (name.length > 0 && email.length > 0 && message.length > 0 && validEmail === true) {
-    errorMsg.innerHTML = '';
-    resetBorder('name');
-    nameValid = 2;
-  } else if (name.length > 0 && email.length > 0 && validEmail === false) {
-    resetBorder('name');
-    errorMsg.innerHTML = 'Invalid email address!<br>';
-    messageValid = 2;
+    updateErrorMsg();
   } else if (name.length > 0) {
     resetBorder('name');
-    messageValid = 2;
+    nameValid = 2;
+    updateErrorMsg();
   }
 }
 
@@ -48,91 +53,54 @@ function checkEmailField() {
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   const message = document.getElementById('message').value;
-  const errorMsg = document.getElementById('error-msg');
   validEmail = (/^(?:[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/).test(email);
-  if (validEmail != true) {
-    setRedBorder('email');
-    errorMsg.innerHTML = 'Invalid email address!<br>';
-    emailValid = 1;
-  } else if (nameValid === 1 || messageValid === 1) {
-    errorMsg.innerHTML = 'Please fill out missing field(s)!<br>';
-    resetBorder('email');
-    emailValid = 2;
-  } else if (validEmail === true && nameValid === 2 && messageValid === 2) {
-    errorMsg.innerHTML = '';
-    resetBorder('email');
-    emailValid = 2;
-  }
   if (email.length === 0) {
     setRedBorder('email');
-    errorMsg.innerHTML = 'Please fill out missing field(s)!<br>';
     emailValid = 1;
-  } else if (name.length > 0 && email.length > 0 && message.length > 0 && validEmail === true && nameValid != 1 && messageValid != 1) {
-    errorMsg.innerHTML = '';
+    updateErrorMsg();
+  } else if (email.length > 0) {
     resetBorder('email');
     emailValid = 2;
-  } else if (email.length > 0 && validEmail === true && nameValid != 1 && messageValid != 1) {
-    errorMsg.innerHTML = '';
-    resetBorder('email');
-    emailValid = 2;
+    updateErrorMsg();
+  }
+  if (validEmail != true && email.length > 0) {
+    setRedBorder('email');
+    emailValid = 1;
+    errorMsg.innerHTML = 'Invalid email address!<br>';
   }
 }
 
 function checkMessageField() {
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
   const message = document.getElementById('message').value;
-  const errorMsg = document.getElementById('error-msg');
   if (message.length === 0) {
     setRedBorder('message');
-    errorMsg.innerHTML = 'Please fill out missing field(s)!<br>';
     messageValid = 1;
-  } else if (name.length > 0 && email.length > 0 && message.length > 0 && validEmail === true) {
-    errorMsg.innerHTML = '';
-    resetBorder('message');
-    messageValid = 2;
-  } else if (name.length > 0 && email.length > 0 && validEmail === false) {
-    resetBorder('message');
-    errorMsg.innerHTML = 'Invalid email address!<br>';
-    messageValid = 2;
+    updateErrorMsg();
   } else if (message.length > 0) {
     resetBorder('message');
     messageValid = 2;
+    updateErrorMsg();
   }
 }
 
-let captchaState = 0; // 0 = not completed, 1 = completed
-
 function recaptchaCompleted() {
-  const errorMsg = document.getElementById('error-msg');
-
-  captchaState = 1;
-
-  if (nameValid === 1 || emailValid === 1 || messageValid === 1) {
-    errorMsg.innerHTML = 'Please fill out missing field(s)!<br>';
-  } else if (nameValid != 1 && emailValid != 1 && messageValid != 1 && validEmail === false) {
-    errorMsg.innerHTML = 'Invalid email address!<br>';
-  } else if (nameValid != 1 && emailValid != 1 && messageValid != 1 && validEmail === true) {
-    errorMsg.innerHTML = '';
-  }
+  captchaState = 2;
+  updateErrorMsg();
 }
 
 function recaptchaError() {
-  const errorMsg = document.getElementById('error-msg');
-  captchaState = 0;
+  captchaState = 1;
   errorMsg.innerHTML = 'An error occurred or the captcha expired, please (re)complete!<br>';
 }
 
 function submitForm() {
-  const errorMsg = document.getElementById('error-msg');
-
   checkNameField();
   checkEmailField();
   checkMessageField();
 
-  if (nameValid === 2 && emailValid === 2 && messageValid === 2 && validEmail === true && captchaState === 1) {
+  if (nameValid === 2 && emailValid === 2 && messageValid === 2 && validEmail === true && captchaState === 2) {
     document.getElementById("contact-form").submit();
-  } else if (captchaState === 0) {
+  } else if (captchaState === 1) {
     errorMsg.innerHTML = 'Please complete the captcha!<br>';
   }
 }
