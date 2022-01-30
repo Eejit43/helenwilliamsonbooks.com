6af8531e12a8e143ace5836a0a77de9b04c77926
Eejit <76887639+Eejit43@users.noreply.github.com>
2022-01-29 21:54:01 -0500

    complete formatting rework

--- contact-form.js
+++ contact-form.js
@@ -1,23 +1,25 @@
 /* Add event listeners */
-document.getElementById('name').addEventListener("input", checkNameField);
-document.getElementById('name').addEventListener("blur", checkNameField);
-document.getElementById('email').addEventListener("input", checkEmailField);
-document.getElementById('email').addEventListener("blur", checkEmailField);
-document.getElementById('message').addEventListener("input", checkMessageField);
-document.getElementById('message').addEventListener("blur", checkMessageField);
-document.getElementById('send').addEventListener("click", submitForm);
+document.getElementById('name').addEventListener('input', checkNameField);
+document.getElementById('name').addEventListener('blur', checkNameField);
+document.getElementById('email').addEventListener('input', checkEmailField);
+document.getElementById('email').addEventListener('blur', checkEmailField);
+document.getElementById('message').addEventListener('input', checkMessageField);
+document.getElementById('message').addEventListener('blur', checkMessageField);
+document.getElementById('send').addEventListener('click', submitForm);
 
 function setRedBorder(id) {
     let element = document.getElementById(id);
-    element.style.border = "1px solid #ff5555";
+    element.style.border = '1px solid #ff5555';
 }
 
 function resetBorder(id) {
     let element = document.getElementById(id);
-    element.style.border = "";
+    element.style.border = '';
 }
 
-let nameValid, emailValid, messageValid = 0; // 0 = unset, 1 = not valid, 2 = valid
+let nameValid,
+    emailValid,
+    messageValid = 0; // 0 = unset, 1 = not valid, 2 = valid
 let validEmail; // true/false
 let captchaState = 0; // 0 = not completed, 1 = error, 2 = completed
 
@@ -54,7 +56,10 @@ function checkEmailField() {
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
-    validEmail = (/^(?:[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/).test(email);
+    validEmail =
+        /^(?:[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/.test(
+            email
+        );
     if (email.length === 0) {
         setRedBorder('email');
         emailValid = 1;
@@ -100,7 +105,7 @@ function submitForm() {
     checkMessageField();
 
     if (nameValid === 2 && emailValid === 2 && messageValid === 2 && validEmail === true && captchaState === 2) {
-        document.getElementById("contact-form").submit();
+        document.getElementById('contact-form').submit();
     } else {
         updateErrorMsg();
     }
--- image-popup.js
+++ image-popup.js
@@ -2,24 +2,24 @@
 
 let modal = document.getElementById('modal');
 let images = document.getElementsByClassName('popupimage');
-let modalImg = document.getElementById("enlarged-image");
-let captionText = document.getElementById("caption");
+let modalImg = document.getElementById('enlarged-image');
+let captionText = document.getElementById('caption');
 
 for (let i = 0; i < images.length; i++) {
     let img = images[i];
-    img.onclick = function (evt) { // jshint ignore:line
-        modal.style.display = "block";
+    img.onclick = function (evt) {
+        modal.style.display = 'block';
         modalImg.src = this.src;
         captionText.innerHTML = this.alt;
     };
 }
 
-let span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName('close')[0];
 
 span.onclick = function () {
-    modal.style.display = "none";
+    modal.style.display = 'none';
 };
 
 modal.onclick = function () {
-    modal.style.display = "none";
+    modal.style.display = 'none';
 };
